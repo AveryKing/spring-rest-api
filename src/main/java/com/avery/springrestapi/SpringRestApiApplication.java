902a5ff38f62cc69e43752e800948fe62b99900b
Avery King <kavery086@gmail.com>
2022-03-29 22:36:02 -0400

    initial

---
+++ src/main/java/com/avery/springrestapi/SpringRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.avery.springrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestApiApplication.class, args);
+    }
+
+}
